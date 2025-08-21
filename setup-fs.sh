3f968630b8dd0847ef4b992a48de2b9bae43c414
Branislav Trstenský <bt7s7k7@hotmail.sk>
2025-08-21 18:29:57 +0200

    Cleaned up setup-fs.sh

--- setup-fs.sh
+++ setup-fs.sh
@@ -1,11 +1,6 @@
-#mkdir overlay.local
-#mkdir overlay.local/upper
-#mkdir overlay.local/work
-#mkdir bin
-#
-#fuse-overlayfs -o lowerdir=build/classes/java:build/resources,upperdir=overlay.local/upper,workdir=overlay.local/work bin
+#!/bin/bash
 
-while [ 1 ]
+while true
 do
     sleep 1
 	cp .vscode/launch.bak.json .vscode/launch.json
