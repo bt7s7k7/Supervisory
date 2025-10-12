a28448faeef3f34cf3711244b138b721377b3adf
Branislav Trstenský <bt7s7k7@hotmail.sk>
2025-10-12 15:02:10 +0200

    Adding tag to blocks to allow Create wrench pickup

--- src/main/java/bt7s7k7/supervisory/blocks/AllBlocks.java
+++ src/main/java/bt7s7k7/supervisory/blocks/AllBlocks.java
@@ -5,11 +5,17 @@ import com.tterrag.registrate.util.entry.BlockEntry;
 import bt7s7k7.supervisory.Supervisory;
 import bt7s7k7.supervisory.blocks.programmableLogicController.ProgrammableLogicControllerBlock;
 import bt7s7k7.supervisory.blocks.remoteTerminalUnit.RemoteTerminalUnitBlock;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 
 public class AllBlocks {
+	private static final TagKey<Block> WRENCH_PICKUP = TagKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath("create", "wrench_pickup"));
+
 	public static final BlockEntry<RemoteTerminalUnitBlock> REMOTE_TERMINAL_UNIT = Supervisory.REGISTRATE.block("remote_terminal_unit", RemoteTerminalUnitBlock::new)
-			.initialProperties(() -> Blocks.STONE)
+			.initialProperties(() -> Blocks.DROPPER)
 			.simpleItem()
 			.blockstate((ctx, prov) -> prov.horizontalBlock(ctx.getEntry(),
 					prov.models().orientableWithBottom(
@@ -18,10 +24,11 @@ public class AllBlocks {
 							prov.modLoc("block/" + ctx.getName() + "_front"),
 							prov.mcLoc("minecraft:block/furnace_top"),
 							prov.mcLoc("minecraft:block/smithing_table_top"))))
+			.tag(WRENCH_PICKUP)
 			.register();
 
 	public static final BlockEntry<ProgrammableLogicControllerBlock> PROGRAMMABLE_LOGIC_CONTROLLER = Supervisory.REGISTRATE.block("programmable_logic_controller", ProgrammableLogicControllerBlock::new)
-			.initialProperties(() -> Blocks.STONE)
+			.initialProperties(() -> Blocks.DROPPER)
 			.simpleItem()
 			.blockstate((ctx, prov) -> prov.horizontalBlock(ctx.getEntry(),
 					prov.models().orientableWithBottom(
@@ -30,6 +37,7 @@ public class AllBlocks {
 							prov.modLoc("block/" + ctx.getName() + "_front"),
 							prov.mcLoc("minecraft:block/furnace_top"),
 							prov.mcLoc("minecraft:block/beehive_end"))))
+			.tag(WRENCH_PICKUP)
 			.register();
 
 	public static void register() {
