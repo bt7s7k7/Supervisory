e59150e95b75d2ede54f805e6ed85e009e798006
Branislav Trstenský <bt7s7k7@hotmail.sk>
2025-12-21 18:57:38 +0100

    Recipes and creative tabs for items

--- src/main/java/bt7s7k7/supervisory/blocks/AllBlocks.java
+++ src/main/java/bt7s7k7/supervisory/blocks/AllBlocks.java
@@ -1,16 +1,26 @@
 package bt7s7k7.supervisory.blocks;
 
+import com.tterrag.registrate.providers.RegistrateRecipeProvider;
+import com.tterrag.registrate.util.DataIngredient;
 import com.tterrag.registrate.util.entry.BlockEntry;
 
 import bt7s7k7.supervisory.Supervisory;
 import bt7s7k7.supervisory.blocks.programmableLogicController.ProgrammableLogicControllerBlock;
 import bt7s7k7.supervisory.blocks.remoteTerminalUnit.RemoteTerminalUnitBlock;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.data.recipes.RecipeCategory;
+import net.minecraft.data.recipes.ShapedRecipeBuilder;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 
+@EventBusSubscriber
 public class AllBlocks {
 	private static final TagKey<Block> WRENCH_PICKUP = TagKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath("create", "wrench_pickup"));
 
@@ -25,6 +35,22 @@ public class AllBlocks {
 							prov.mcLoc("minecraft:block/furnace_top"),
 							prov.mcLoc("minecraft:block/smithing_table_top"))))
 			.tag(WRENCH_PICKUP)
+			.item()
+			.recipe((ctx, prov) -> {
+				prov.singleItem(DataIngredient.items(AllBlocks.PROGRAMMABLE_LOGIC_CONTROLLER.asItem()), RecipeCategory.REDSTONE, ctx::getEntry, 1, 1);
+
+				ShapedRecipeBuilder.shaped(RecipeCategory.REDSTONE, ctx.getEntry())
+						.define('I', Items.IRON_INGOT)
+						.define('R', Items.REDSTONE)
+						.define('C', Items.COPPER_BLOCK)
+						.define('S', Items.STONE)
+						.pattern("III")
+						.pattern("RCR")
+						.pattern("SSS")
+						.unlockedBy("has_copper", RegistrateRecipeProvider.has(Items.COPPER_INGOT))
+						.save(prov, prov.safeId(ctx.getEntry()).withSuffix("_full"));
+			})
+			.build()
 			.register();
 
 	public static final BlockEntry<ProgrammableLogicControllerBlock> PROGRAMMABLE_LOGIC_CONTROLLER = Supervisory.REGISTRATE.block("programmable_logic_controller", ProgrammableLogicControllerBlock::new)
@@ -38,9 +64,22 @@ public class AllBlocks {
 							prov.mcLoc("minecraft:block/furnace_top"),
 							prov.mcLoc("minecraft:block/beehive_end"))))
 			.tag(WRENCH_PICKUP)
+			.item()
+			.recipe((ctx, prov) -> {
+				prov.singleItem(DataIngredient.items(REMOTE_TERMINAL_UNIT.asItem()), RecipeCategory.REDSTONE, ctx::getEntry, 1, 1);
+			})
+			.build()
 			.register();
 
 	public static void register() {
 		// Load this class
+	}
+
+	@SubscribeEvent
+	private static void handleCreativeTabs(BuildCreativeModeTabContentsEvent event) {
+		if (event.getTabKey() == CreativeModeTabs.REDSTONE_BLOCKS) {
+			event.accept(PROGRAMMABLE_LOGIC_CONTROLLER);
+			event.accept(REMOTE_TERMINAL_UNIT);
+		}
 	}
 }
