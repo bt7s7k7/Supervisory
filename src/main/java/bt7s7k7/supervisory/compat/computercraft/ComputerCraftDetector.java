515f056157ebec2cb1d8aa3414774a94a309c81e
Branislav Trstenský <bt7s7k7@hotmail.sk>
2025-12-24 14:16:02 +0100

    Fixed class not found when launching without CC

--- src/main/java/bt7s7k7/supervisory/compat/computercraft/ComputerCraftDetector.java
+++ src/main/java/bt7s7k7/supervisory/compat/computercraft/ComputerCraftDetector.java
@@ -1,16 +1,9 @@
 package bt7s7k7.supervisory.compat.computercraft;
 
-import bt7s7k7.supervisory.Supervisory;
 import bt7s7k7.supervisory.Support;
-import bt7s7k7.supervisory.blocks.AllBlockEntities;
-import bt7s7k7.supervisory.composition.CompositeBlockEntity;
-import bt7s7k7.supervisory.system.ScriptedSystemInitializationEvent;
-import dan200.computercraft.api.peripheral.PeripheralCapability;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLConstructModEvent;
-import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
-import net.neoforged.neoforge.common.NeoForge;
 
 @EventBusSubscriber
 public final class ComputerCraftDetector {
@@ -18,26 +11,6 @@ public final class ComputerCraftDetector {
 
 	@SubscribeEvent
 	public static void detectComputerCraft(FMLConstructModEvent event) {
-		Support.executeIfModInstalled("computercraft", () -> ComputerCraftDetector::initializeCompat);
-	}
-
-	private static void initializeCompat() {
-		Supervisory.LOGGER.info("Initializing CC: Tweaked compatibility");
-		var bus = Supervisory.getInstance().bus;
-
-		bus.addListener((RegisterCapabilitiesEvent event) -> {
-			event.registerBlockEntity(PeripheralCapability.get(), AllBlockEntities.PROGRAMMABLE_LOGIC_CONTROLLER.get(), (blockEntity, context) -> {
-				if (!(blockEntity instanceof CompositeBlockEntity composite)) return null;
-
-				var host = composite.getComponent(PeripheralConnectionController.class);
-				if (!host.isPresent()) return null;
-
-				return new ScriptedSystemPeripheral(host.get());
-			});
-		});
-
-		NeoForge.EVENT_BUS.addListener((ScriptedSystemInitializationEvent init) -> {
-			init.entity.addComponent(new PeripheralConnectionController(init.entity, init.systemHost));
-		});
+		Support.executeIfModInstalled("computercraft", () -> PeripheralConnectionController::initializeCompat);
 	}
 }
--- src/main/java/bt7s7k7/supervisory/compat/computercraft/PeripheralConnectionController.java
+++ src/main/java/bt7s7k7/supervisory/compat/computercraft/PeripheralConnectionController.java
@@ -1,13 +1,19 @@
 package bt7s7k7.supervisory.compat.computercraft;
 
+import bt7s7k7.supervisory.Supervisory;
 import bt7s7k7.supervisory.Support;
+import bt7s7k7.supervisory.blocks.AllBlockEntities;
 import bt7s7k7.supervisory.composition.BlockEntityComponent;
 import bt7s7k7.supervisory.composition.CompositeBlockEntity;
 import bt7s7k7.supervisory.system.ScriptedSystemHost;
+import bt7s7k7.supervisory.system.ScriptedSystemInitializationEvent;
 import dan200.computercraft.api.peripheral.AttachedComputerSet;
+import dan200.computercraft.api.peripheral.PeripheralCapability;
 import dan200.computercraft.core.ComputerContext;
 import dan200.computercraft.shared.computer.core.ServerContext;
 import net.minecraft.server.level.ServerLevel;
+import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
+import net.neoforged.neoforge.common.NeoForge;
 
 public class PeripheralConnectionController extends BlockEntityComponent {
 	public final ScriptedSystemHost systemHost;
@@ -38,6 +44,26 @@ public class PeripheralConnectionController extends BlockEntityComponent {
 			PeripheralConnection.WRAPPER.ensurePrototype(realm);
 
 			event.system().integrations.putInstance(InteropAPI.class, interop);
+		});
+	}
+
+	public static void initializeCompat() {
+		Supervisory.LOGGER.info("Initializing CC: Tweaked compatibility");
+		var bus = Supervisory.getInstance().bus;
+
+		bus.addListener((RegisterCapabilitiesEvent event) -> {
+			event.registerBlockEntity(PeripheralCapability.get(), AllBlockEntities.PROGRAMMABLE_LOGIC_CONTROLLER.get(), (blockEntity, context) -> {
+				if (!(blockEntity instanceof CompositeBlockEntity composite)) return null;
+
+				var host = composite.getComponent(PeripheralConnectionController.class);
+				if (!host.isPresent()) return null;
+
+				return new ScriptedSystemPeripheral(host.get());
+			});
+		});
+
+		NeoForge.EVENT_BUS.addListener((ScriptedSystemInitializationEvent init) -> {
+			init.entity.addComponent(new PeripheralConnectionController(init.entity, init.systemHost));
 		});
 	}
 }
